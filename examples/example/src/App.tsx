67d25f2358e36ee14e5b01302f0b5d394c791951
Grady <grady.r.mccallum@gmail.com>
2022-11-18 22:29:45 +1300

    Made package + example project

---
+++ examples/example/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='ExampleText'>
+        <h2>This is an example of a toast</h2>
+        <button>Open Toast</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
