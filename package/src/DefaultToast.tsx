8a8db9090b55cce53b3260c0e3766caed6cd793a
Grady <grady.r.mccallum@gmail.com>
2022-11-19 19:58:47 +1300

    Improved styles for default toast

--- package/src/DefaultToast.tsx
+++ package/src/DefaultToast.tsx
@@ -14,18 +14,29 @@ function DefaultToast({ color, text }: any) {
             bottom: '20px',
             transform: 'translate(-50%, -50%)',
             margin: '0 auto',
+            //border: '2px solid grey',
+            borderRadius: '0.5rem',
+            color: 'white',
+            maxWidth: '25em',
+            fontSize: '16pt',
+            boxShadow: '1px 0px 4px 1px #000000A0'
         }}>
             <div
                 onClick={() => toast.closeToast()}
                 style={{
                     position: 'absolute',
-                    right: '5px',
+                    right: '0.5em',
+                    top: '',
+                    float: 'right',
+                    textShadow: '0 1px 0 #fff',
                     cursor: 'pointer'
                 }}>x</div>
             <div
                 style={{
-                    padding: '0.5rem',
-                    fontSize: '1.5rem'
+                    padding: '0.5em',
+                    marginRight: '1em',
+                    fontWeight: '400',
+                    textAlign: 'left'
                 }}>{text}</div>
         </div>
     )
